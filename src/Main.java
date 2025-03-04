50cd61ca2df4534a8e713b0f76b6910073783891
Slavik <rutkovsky2003@gmail.com>
2025-03-04 15:57:09 -0800

    connected the database

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
+import java.sql.*;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        String url = "jdbc:mysql://localhost:3306/project10_db";
+        String username = "root";
+        String password = "1231";
+
+        try (Connection connection = DriverManager.getConnection(url, username, password)) {
+            System.out.println("Database connected!");
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("select * from trail");
+            while (resultSet.next()) {
+                System.out.println(resultSet.getString("trail_id") + "\t"
+                        + resultSet.getString("trail_name") + "\t"
+                        + resultSet.getString("trail_length") + "\t"
+                        + resultSet.getString("trail_difficulty") + "\t"
+                        + resultSet.getString("trail_rating") + "\t"
+                        + resultSet.getString("trail_estimated_time") + "\t");
+            }
+        } catch (SQLException e) {
+            throw new IllegalStateException("Cannot connect the database!", e);
+        }
     }
 }

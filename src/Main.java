c69b0d094fbc208951a471c3facb6e022635c3c6
Slavik <rutkovsky2003@gmail.com>
2025-03-04 21:02:33 -0800

    added main UI elements, runQuery method, 2 buttons to view trail and user tables

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,81 @@
+import java.awt.FlowLayout;
 import java.sql.*;
+import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
 
 public class Main {
-    public static void main(String[] args) {
+    private static final Connection connection;
+
+    static {
+        try {
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/project10_db",
+                    "root", "1231");
+            System.out.println("Database connected!");
+        } catch (SQLException e) {
+            System.out.println("Connection failed!");
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void main(String[] args){
         System.out.println("Hello world!");
 
-        String url = "jdbc:mysql://localhost:3306/project10_db";
-        String username = "root";
-        String password = "1231";
+        JFrame frame = new JFrame();
+        frame.setLayout(new FlowLayout(FlowLayout.CENTER));
+        frame.setSize(500, 600);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        JTable table = new JTable();
 
-        try (Connection connection = DriverManager.getConnection(url, username, password)) {
-            System.out.println("Database connected!");
-            Statement statement = connection.createStatement();
-            ResultSet resultSet = statement.executeQuery("select * from trail");
-            while (resultSet.next()) {
-                System.out.println(resultSet.getString("trail_id") + "\t"
-                        + resultSet.getString("trail_name") + "\t"
-                        + resultSet.getString("trail_length") + "\t"
-                        + resultSet.getString("trail_difficulty") + "\t"
-                        + resultSet.getString("trail_rating") + "\t"
-                        + resultSet.getString("trail_estimated_time") + "\t");
+        JButton trailButton = new JButton("View Trails");
+        trailButton.setSize(200, 100);
+        JButton userButton = new JButton("View Users");
+        userButton.setSize(200, 100);
+
+        frame.add(new JScrollPane(table));
+        frame.add(trailButton);
+        frame.add(userButton);
+        frame.setVisible(true);
+
+
+
+        trailButton.addActionListener(e -> {
+            try {
+                DefaultTableModel model = runQuery("SELECT * FROM trail");
+                table.setModel(model);
+            } catch (SQLException ex) {
+                throw new RuntimeException(ex);
             }
-        } catch (SQLException e) {
-            throw new IllegalStateException("Cannot connect the database!", e);
+        });
+        userButton.addActionListener(e -> {
+            try {
+                DefaultTableModel model = runQuery("SELECT * FROM user");
+                table.setModel(model);
+            } catch (SQLException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+
+
         }
+    private static DefaultTableModel runQuery(String sqlQuery) throws SQLException {
+
+        Statement statement = connection.createStatement();
+        ResultSet table_data = statement.executeQuery(sqlQuery);
+        ResultSetMetaData table_cols = table_data.getMetaData();
+        String[] cols = new String[table_cols.getColumnCount()];
+        for (int i = 0; i < table_cols.getColumnCount(); i++) {
+            cols[i] = table_cols.getColumnName(i + 1);
+        }
+        DefaultTableModel model = new DefaultTableModel();
+        model.setColumnIdentifiers(cols);
+        while (table_data.next()) {
+            String[] row = new String[cols.length];
+            for (int i = 0; i < cols.length; i++) {
+                row[i] = table_data.getString(i + 1);
+            }
+            model.addRow(row);
+        }
+        return model;
     }
 }
